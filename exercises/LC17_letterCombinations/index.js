29e42ac078ec19f956c2afddab0b31f87db886e8
Julian Chen <julian9925@gmail.com>
2019-12-01 19:33:39 +0800

    finish LC17, LC70

--- exercises/LC17_letterCombinations/index.js
+++ exercises/LC17_letterCombinations/index.js
@@ -1,14 +1,48 @@
-var letterCombinations = function(digits) {
-  const hashMap = {
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"]
-  };
+var letterCombinations = function (digits) {
+    const hashMap = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+    };
+
+    let target = [];
+    var i;
+    for (i in digits) {
+        target.push(hashMap[digits[i]]);
+    }
+
+    return combinationRecur(target);
 };
 
+function combinationRecur(arr) {
+    let newArr = Object.assign([], arr);
+    let top;
+    let res;
+    let ret;
+    if (newArr.length === 0) {
+        return [];
+    } else {
+        res = [];
+        top = newArr.shift();
+        ret = combinationRecur(newArr);
+        if (ret.length === 0) {
+            res = top;
+        } else {
+            for (let i = 0; i < top.length; i++) {
+                for (let j = 0; j < ret.length; j++) {
+                    res.push(top[i] + ret[j]);
+                }
+            }
+        }
+        return res;
+    }
+}
+
 module.exports = letterCombinations;
+
+
--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,3 +1,18 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+	if (n <= 3) {
+		return n;
+	}
+
+	let first = 1, second = 2;
+	let third;
+
+	for (let i = 3; i <= n; i ++) {
+		third = first + second;
+		first = second;
+		second = third;
+	}
+
+	return third;
+};
 
 module.exports = climbStairs;
