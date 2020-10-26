fbb3774f561f66c878c312f51d4bb7eb2c753706
Julian Chen <julian9925@gmail.com>
2020-10-26 19:54:10 +0800

    add LC121 LC32 LC3

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -1,3 +1,14 @@
-var maxProfit = function(prices) {};
+function maxProfit(nums) {
+	let maxProfit = 0;
+	let bestBuyPrice = nums[0];
+
+	for (let index = 0; index < nums.length; index++) {
+		const currentPrice = nums[index];
+		bestBuyPrice = (currentPrice < bestBuyPrice) ? currentPrice : bestBuyPrice;
+		maxProfit = Math.max(maxProfit, currentPrice - bestBuyPrice);
+	}
+
+	return maxProfit;
+}
 
 module.exports = maxProfit;
--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,24 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+	let windowHashMap = {};
+	let windowStart = 0;
+	let maxLength = 0;
+
+	for (let i = 0; i< s.length; i++) {
+		const endChar = s[i];
+
+		if (windowHashMap[endChar] >= windowStart) {
+			windowStart = windowHashMap[endChar] + 1;
+		}
+
+		windowHashMap[endChar] = i;
+		maxLength = Math.max(maxLength, i - windowStart + 1);
+	}
+
+	return maxLength;
+}
+
 
 module.exports = lengthOfLongestSubstring;
