e921012fd0a1109d71baf60730e393d77e072f09
Julian Chen <julian9925@gmail.com>
2022-10-05 20:42:17 +0800

    feat: add merged sorted list

--- exercises/LC21_mergeTwoLists/index.js
+++ exercises/LC21_mergeTwoLists/index.js
@@ -1,3 +1,31 @@
-function mergeTwoLists(l1, l2) {}
-
+var ListNode = /** @class */ (function () {
+    function ListNode(val, next) {
+        if (next === void 0) { next = null; }
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+    return ListNode;
+}());
+function mergeTwoLists(l1, l2) {
+    var mergeList = new ListNode(0);
+    var current = mergeList;
+    while (l1 !== null && l2 != null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        }
+        else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+    if (l1 !== null) {
+        current.next = l1;
+    }
+    if (l2 !== null) {
+        current.next = l2;
+    }
+    return mergeList.next;
+}
 module.exports = mergeTwoLists;
