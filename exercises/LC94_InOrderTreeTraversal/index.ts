927fe582ab805cbf1ad4f06c1db360e72d193446
Julian Chen <julian9925@gmail.com>
2022-11-09 13:53:39 +0800

    feat: add inorder traversal

---
+++ exercises/LC94_InOrderTreeTraversal/index.ts
@@ -0,0 +1,43 @@
+//   Definition for a binary tree node.
+  class TreeNode {
+      val: number
+      left: TreeNode | null
+      right: TreeNode | null
+      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+          this.val = (val === undefined ? 0 : val)
+          this.left = (left === undefined ? null : left)
+          this.right = (right === undefined ? null : right)
+      }
+  }
+
+function inorderTraversal(root: TreeNode | null): number[] {
+	const nodeStack: TreeNode[] = [];
+	let result: number[] = [];
+	if (!root) return result;
+
+	let curretNode = root;
+
+	while (!!curretNode || nodeStack.length > 0) {
+		
+		if (!!curretNode) {
+			nodeStack.push(curretNode);
+			curretNode = curretNode.left as TreeNode;
+		} else {
+			const node = nodeStack.pop() as TreeNode;
+			result.push(node.val);
+			curretNode = node.right as TreeNode;
+		}
+	}
+	
+
+
+	return result;
+};
+
+// function inOrder(root: TreeNode) { 
+// 	root.left && inOrder(root.left) 
+// 	result.push(root.val) 
+// 	root.right && inOrder(root.right) 
+// }
+
+// inOrder(root);
