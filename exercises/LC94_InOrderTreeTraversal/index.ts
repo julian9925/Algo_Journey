579dfe9b4ddaf945ada340115cba3cc4f906f547
Julian Chen <julian9925@gmail.com>
2022-11-16 07:13:00 +0800

    feat: add preorder traversal

--- exercises/LC94_InOrderTreeTraversal/index.ts
+++ exercises/LC94_InOrderTreeTraversal/index.ts
@@ -1,14 +1,14 @@
 //   Definition for a binary tree node.
-  class TreeNode {
-      val: number
-      left: TreeNode | null
-      right: TreeNode | null
-      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-          this.val = (val === undefined ? 0 : val)
-          this.left = (left === undefined ? null : left)
-          this.right = (right === undefined ? null : right)
-      }
-  }
+//   class TreeNode {
+//       val: number
+//       left: TreeNode | null
+//       right: TreeNode | null
+//       constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//           this.val = (val === undefined ? 0 : val)
+//           this.left = (left === undefined ? null : left)
+//           this.right = (right === undefined ? null : right)
+//       }
+//   }
 
 function inorderTraversal(root: TreeNode | null): number[] {
 	const nodeStack: TreeNode[] = [];
@@ -20,9 +20,11 @@ function inorderTraversal(root: TreeNode | null): number[] {
 	while (!!curretNode || nodeStack.length > 0) {
 		
 		if (!!curretNode) {
+			// continue to left
 			nodeStack.push(curretNode);
 			curretNode = curretNode.left as TreeNode;
 		} else {
+			// return last visited tree
 			const node = nodeStack.pop() as TreeNode;
 			result.push(node.val);
 			curretNode = node.right as TreeNode;
@@ -33,6 +35,34 @@ function inorderTraversal(root: TreeNode | null): number[] {
 
 	return result;
 };
+
+
+function prerderTraversal(root: TreeNode | null): number[] {
+	const nodeStack: TreeNode[] = [];
+	let result: number[] = [];
+	if (!root) return result;
+
+	let curretNode = root;
+
+	while (!!curretNode || nodeStack.length > 0) {
+		
+		if (!!curretNode) {
+			// continue to left
+			result.push(curretNode.val);
+			nodeStack.push(curretNode);
+			curretNode = curretNode.left as TreeNode;
+		} else {
+			// return last visited tree
+			const node = nodeStack.pop() as TreeNode;
+			curretNode = node.right as TreeNode;
+		}
+	}
+	
+
+
+	return result;
+};
+
 
 // function inOrder(root: TreeNode) { 
 // 	root.left && inOrder(root.left) 
